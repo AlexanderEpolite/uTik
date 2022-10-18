60c48e54119247a46b1e4044b6977ff2c710d781
Alexander Epolite <alexander@epolite.net>
2022-10-17 18:15:36 -0700

    command exists?

--- src/uTik.ts
+++ src/uTik.ts
@@ -2,6 +2,16 @@
 
 import {Master} from "discord-rose";
 import { join } from "path";
+import {sync as commandExists} from "command-exists";
+
+if(!commandExists("yt-dlp")) {
+    console.error(`===================================================`);
+    console.error(`               YT-DLP WAS NOT FOUND!               `);
+    console.error(`Please download it from the link in the repository.`);
+    console.error(`If it is downloaded, ensure that it is in your PATH`);
+    console.error(`===================================================`);
+    process.exit(1);
+}
 
 const secrets = require("../secrets.json");
 
