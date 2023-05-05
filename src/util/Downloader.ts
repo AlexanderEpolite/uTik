32f339caf5aa908a4ced5e14c87aed541c7538f9
Alexander Epolite <alexander@epolite.net>
2023-05-05 05:10:44 +0000

    New Discord upload limit (REAL 2023)

--- src/util/Downloader.ts
+++ src/util/Downloader.ts
@@ -34,8 +34,8 @@ export default class Downloader {
                 //get the size of the file
                 const stats = statSync(`/tmp/${identifier}.mp4`);
                 
-                //if file is greater than 7.9MB, compress it
-                if(stats.size > 8192000) {
+                //if file is greater than 24MB, compress it
+                if(stats.size > 25165820) {
                     worker.api.messages.edit(channel_id, identifier, "Compressing video (this may take a few seconds)");
                     exec(`ffmpeg -i /tmp/${identifier}.mp4 -c:v libx264 -crf 30 -preset slow /tmp/${identifier}-cr.mp4`, (error) => {
                         if(error) {
