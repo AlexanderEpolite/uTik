bce9d4f191690b289588cefdd5d93e92b4fab690
Alexander Epolite <alexander@epolite.net>
2023-08-26 23:40:45 -0700

    redirect stderr from cropper to nothing

--- cropper.bash
+++ cropper.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -i /tmp/${1}.mp4 -t 1 -vf cropdetect -f null - | awk '/crop/ { print $NF }' | tail -1
+ffmpeg -i /tmp/${1}.mp4 -t 1 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1
